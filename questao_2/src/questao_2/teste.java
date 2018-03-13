0e4a5dca9ecdf55ca2ab5991cadcbd3de954af58
Adriner Andrade <adriner_andrade@hotmail.com>
2018-03-12 23:34:40 -0300

    Aceitando null na compactação

--- questao_2/src/questao_2/teste.java
+++ questao_2/src/questao_2/teste.java
@@ -3,7 +3,7 @@ package questao_2;
 public class teste {
 
 	public static void main(String[] args) {
-		Compactador compactador = new Compactador("E aí cara, blz?");
+		Compactador compactador = new Compactador("E aewwwww, deu boa Aurélioo!!!");
 		compactador.compactar();
 		Descompactador descompactador = new Descompactador(compactador.getCompactados(), compactador.getArvore());
 		System.out.println(descompactador.descompactar());
