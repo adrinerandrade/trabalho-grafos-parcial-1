74b8de07ca51e7e745ebbb911a96018549876f3d
Luan <luancarlospurim@gmail.com>
2018-03-22 22:24:57 -0300

    ajuste

--- questao_1/src/questao_1/Leitor.java
+++ questao_1/src/questao_1/Leitor.java
@@ -1,5 +1,10 @@
 package questao_1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -8,15 +13,36 @@ public class Leitor {
 	private static Dicionario dicionario = new Dicionario();
 
 	public static void main(String[] args) {
-		System.out.println("Insira um conteúdo, e digite \"##fim\" em uma linha para sinalizar o fim da operação.");
+		System.out.println("Digite o caminho para o arquivo selecionado.");
 		Scanner keyboard = new Scanner(System.in);
-		StringBuilder content = new StringBuilder();
+		String caminho = "";  
+		File arquivo;
+		do {
+			caminho = keyboard.nextLine();			
+			arquivo = new File(caminho);
+			
+			if(!arquivo.exists()) {
+				System.out.println("Arquivo não encontrado. Insira novamente.");
+				caminho = "";
+			}
+			
+		} while (caminho.isEmpty());
+		
 		String line;
-		while (!"##fim".equals((line = keyboard.nextLine()))) {
-			content.append(line);
-			content.append("\n");
-		}
-		keyboard.close();
+		StringBuilder content = new StringBuilder();
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(arquivo));
+			while((line = reader.readLine()) != null) {
+				if(content.length() > 0) content.append(System.lineSeparator());
+				content.append(line);
+			}   				
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}				
 
 		dicionario.reconhecerPalavras(content.toString());
 
